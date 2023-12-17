cf3a8cc9441a1e33f84c5ee0afc32074be070095
huynhhoc <huynhhoc@gmail.com>
2023-12-17 10:55:20 +0100

    refactor code

--- main.py
+++ main.py
@@ -27,15 +27,32 @@ def Collect_existing_directories(replica_path):
             existing_files_in_replica.add(file_path_replica)
     return existing_dirs_in_replica, existing_files_in_replica
 #--------------------------------------------------------------------------------
-def remove_file(existing_files_in_replica, logging):
+def remove_files(existing_files_in_replica, logging):
     for file_path_replica_to_remove in existing_files_in_replica:
         logging.info(f"Removing file: {file_path_replica_to_remove}")
         os.remove(file_path_replica_to_remove)
 #--------------------------------------------------------------------------------
-def remove_dir(existing_dirs_in_replica):
+def remove_dirs(existing_dirs_in_replica):
     for dir_path_replica_to_remove in existing_dirs_in_replica:
         logging.info(f"Removing directory: {dir_path_replica_to_remove}")
         shutil.rmtree(dir_path_replica_to_remove)
+#--------------------------------------------------------------------------------
+def check_if_file_need_update(source_state, tasks, file_path_replica, file_path_src):
+    #3.2. Check if the file needs to be updated
+    replica_stat = (os.stat(file_path_replica).st_mtime, os.stat(file_path_replica).st_size)
+    src_stat = (os.stat(file_path_src).st_mtime, os.stat(file_path_src).st_size)
+    if source_state.get(file_path_src) != src_stat and replica_stat != src_stat:
+        logging.info(f"Updating file: {file_path_replica}")
+        #3.3. Asynchronous File Copy
+        tasks.append(async_copy_file(file_path_src, file_path_replica))
+        # Update source state after confirming the file exists in the replica
+        source_state[file_path_src] = src_stat
+#--------------------------------------------------------------------------------
+def create_folder_if_not_exist(dir_path_replica):
+    #If the directory does not exist in the replica, create it.
+    if not os.path.exists(dir_path_replica):
+        logging.info(f"Creating directory: {dir_path_replica}")
+        os.makedirs(dir_path_replica, exist_ok=True)
 #--------------------------------------------------------------------------------
 # Asynchronous function to synchronize folders between source and replica
 async def async_synchronize_folders(source_path, replica_path, logging, source_state):
@@ -47,10 +64,7 @@ async def async_synchronize_folders(source_path, replica_path, logging, source_s
             #For each directory found, construct the corresponding directory path in the replica folder
             dir_path_replica = dir_path_src.replace(source_path, replica_path)
             #If the directory does not exist in the replica, create it.
-            if not os.path.exists(dir_path_replica):
-                logging.info(f"Creating directory: {dir_path_replica}")
-                os.makedirs(dir_path_replica, exist_ok=True)
-
+            create_folder_if_not_exist(dir_path_replica)
     tasks = []
     #2. Collect existing directories and files in the replica
     existing_dirs_in_replica, existing_files_in_replica = Collect_existing_directories(replica_path)
@@ -62,12 +76,10 @@ async def async_synchronize_folders(source_path, replica_path, logging, source_s
             dir_path_replica = dir_path_src.replace(source_path, replica_path)
             # Remove file from the set of existing files in the replica
             existing_dirs_in_replica.discard(dir_path_replica)
-        
         for file_name in files:
             file_path_src = os.path.join(root, file_name)
             #For each source file, construct the corresponding path in the ``replica``
             file_path_replica = file_path_src.replace(source_path, replica_path)
-
             #3.1. Check if the file exists in the replica
             if not os.path.exists(file_path_replica):
                 logging.info(f"Copying new file: {file_path_replica}")
@@ -75,22 +87,15 @@ async def async_synchronize_folders(source_path, replica_path, logging, source_s
                 tasks.append(async_copy_file(file_path_src, file_path_replica))
             else:
                 #3.2. Check if the file needs to be updated
-                replica_stat = (os.stat(file_path_replica).st_mtime, os.stat(file_path_replica).st_size)
-                src_stat = (os.stat(file_path_src).st_mtime, os.stat(file_path_src).st_size)
-                if source_state.get(file_path_src) != src_stat and replica_stat != src_stat:
-                    logging.info(f"Updating file: {file_path_replica}")
-                    #3.3. Asynchronous File Copy
-                    tasks.append(async_copy_file(file_path_src, file_path_replica))
-                    # Update source state after confirming the file exists in the replica
-                    source_state[file_path_src] = src_stat
+                check_if_file_need_update(source_state, tasks, file_path_replica, file_path_src)
 
             # Remove file from the set of existing files in the replica
             existing_files_in_replica.discard(file_path_replica)
 
     #4. Remove files in replica that don't exist in the source
-    remove_file(existing_files_in_replica, logging)
+    remove_files(existing_files_in_replica, logging)
     #4. Remove directories in replica that don't exist in the source
-    remove_dir(existing_dirs_in_replica)
+    remove_dirs(existing_dirs_in_replica)
     #6. Synchronize All Operations
     await asyncio.gather(*tasks)
 #--------------------------------------------------------------------------------
